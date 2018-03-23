e51d87729b31ee6676640e991490a3aebced489e
jayanth amirineni <jayasai.amerineni@gmail.com>
2018-03-23 12:57:31 +1100

    added console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindow(IntPtr hWnd, GetWindow_Cmd uCmd);
+        public enum GetWindow_Cmd : uint
+        {
+            GW_HWNDFIRST = 0,
+            GW_HWNDLAST = 1,
+            GW_HWNDNEXT = 2,
+            GW_HWNDPREV = 3,
+            GW_OWNER = 4,
+            GW_CHILD = 5,
+            GW_ENABLEDPOPUP = 6
+        }
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+            ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+            ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool EnumDesktopWindows(IntPtr hDesktop, 
+            EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+        static void Main(string[] args)
+        {
+            var collection = new List<string>();
+            Program.EnumDelegate filter = delegate (IntPtr hWnd, int lParam)
+            {
+                StringBuilder strbTitle = new StringBuilder(255);
+                int nLength = Program.GetWindowText(hWnd, strbTitle, strbTitle.Capacity + 1);
+                string strTitle = strbTitle.ToString();
+
+                if (Program.IsWindowVisible(hWnd) && !string.IsNullOrEmpty(strTitle) && GetWindow(hWnd, GetWindow_Cmd.GW_OWNER) != null)
+                {
+                    collection.Add(strTitle);
+                }
+                return true;
+            };
+
+            if (Program.EnumDesktopWindows(IntPtr.Zero, filter, IntPtr.Zero))
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
--- WinMonSim/Program.cs
+++ WinMonSim/Program.cs
@@ -17,7 +17,7 @@ namespace WinMonSim
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new WinMonService()
             };
             ServiceBase.Run(ServicesToRun);
         }
