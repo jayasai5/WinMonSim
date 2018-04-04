0ed8fe578031f79b9317ad5be3638cb116851892
jayanth amirineni <jayasai.amerineni@gmail.com>
2018-04-04 23:09:30 +1000

    drawing window borders

---
+++ ConsoleApp/Border.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp
+{
+    public partial class Border : Form
+    {
+        public Rectangle InnerRectangle;
+        public Rectangle OuterRectangle;
+        public int BorderWidth;
+        public Color FillColor = Color.Blue;
+        public Color BorderColor = Color.Blue;
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cX, int cY, uint uFlags);
+
+        public Border()
+        {
+            InitializeComponent();
+            BackColor = FillColor;
+            ForeColor = BorderColor;
+            base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            base.MaximizeBox = false;
+            base.MinimizeBox = false;
+            base.ShowInTaskbar = false;
+            base.Text = "";
+            base.StartPosition = FormStartPosition.Manual;
+
+        }
+        public void HighLight(Rectangle rectangle, int borderWidth) {
+            BorderWidth = borderWidth;
+            SetLocation(rectangle);
+            SetWindowPos(this.Handle, IntPtr.Zero, 0, 0, 0, 0, 0x43);
+            Show();
+        }
+
+        private void SetLocation(Rectangle rectangle)
+        {
+            int width = BorderWidth;
+            this.TopMost = false;
+            OuterRectangle = new Rectangle(new Point(0, 0), rectangle.Size + new Size(width * 2, width * 2));
+            InnerRectangle = new Rectangle(new Point(width, width), rectangle.Size);
+            Region region = new Region(OuterRectangle);
+            region.Exclude(InnerRectangle);
+            base.Location = rectangle.Location - new Size(width, width);
+            base.Size = OuterRectangle.Size;
+            base.Region = region;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            Rectangle rect = new Rectangle(OuterRectangle.Left, OuterRectangle.Top, OuterRectangle.Width, OuterRectangle.Height);
+            Rectangle rectangle2 = new Rectangle(InnerRectangle.Left, InnerRectangle.Top, InnerRectangle.Width, InnerRectangle.Height);
+            e.Graphics.DrawRectangle(new Pen(ForeColor), rectangle2);
+            e.Graphics.DrawRectangle(new Pen(ForeColor), rect);
+        }
+        public void Show()
+        {
+            SetWindowPos(base.Handle, IntPtr.Zero, 0, 0, 0, 0, 0x53);
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp
 {
+    /// <summary>
+    /// class to store the window attributes like window name, its coordinates and handle
+    /// </summary>
     class Window {
         public string WindowName { get; set; }
         [StructLayout(LayoutKind.Sequential)]
@@ -19,30 +24,49 @@ namespace ConsoleApp
         }
         public RECT WindowCoordinates { get; set; }
         public IntPtr WindowHandle { get; set; }
-    }
+    } 
+
     class Program
     {
         public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
-        
+        public static int BorderWidth = 7;
+        public IntPtr currentWinHandle;
+        public Rectangle currentWindow;
+        public Thread RefreshBorderThread;
+        public struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public System.Drawing.Point ptMinPosition;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Rectangle rcNormalPosition;
+        }
 
+        const UInt32 SW_HIDE = 0;
+        const UInt32 SW_SHOWNORMAL = 1;
+        const UInt32 SW_NORMAL = 1;
+        const UInt32 SW_SHOWMINIMIZED = 2;
+        const UInt32 SW_SHOWMAXIMIZED = 3;
+        const UInt32 SW_MAXIMIZE = 3;
+        const UInt32 SW_SHOWNOACTIVATE = 4;
+        const UInt32 SW_SHOW = 5;
+        const UInt32 SW_MINIMIZE = 6;
+        const UInt32 SW_SHOWMINNOACTIVE = 7;
+        const UInt32 SW_SHOWNA = 8;
+        const UInt32 SW_RESTORE = 9;
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        public static extern int ShowWindow(IntPtr hWnd, uint nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool IsWindowVisible(IntPtr hWnd);
         [DllImport("user32.dll")]
-        public static extern IntPtr GetWindow(IntPtr hWnd, GetWindow_Cmd uCmd);
-        [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool GetWindowRect(HandleRef hWnd, out Window.RECT lpRect);
-        public enum GetWindow_Cmd : uint
-        {
-            GW_HWNDFIRST = 0,
-            GW_HWNDLAST = 1,
-            GW_HWNDNEXT = 2,
-            GW_HWNDPREV = 3,
-            GW_OWNER = 4,
-            GW_CHILD = 5,
-            GW_ENABLEDPOPUP = 6
-        }
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
         [DllImport("user32.dll", EntryPoint = "GetWindowText",
@@ -55,6 +79,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             var collection = new List<Window>();
+            //filter windows that are not visible and windows that do not have a name
             Program.EnumDelegate filter = delegate (IntPtr hWnd, int lParam)
             {
                 StringBuilder strbTitle = new StringBuilder(255);
@@ -79,11 +104,24 @@ namespace ConsoleApp
             {
                 foreach (var item in collection)
                 {
-                    Console.WriteLine("window Name:"+ item.WindowName+" (x,y): (" + item.WindowCoordinates.Left + "," + item.WindowCoordinates.Top 
-                        + "),(x,y): (" + item.WindowCoordinates.Right  + "," + item.WindowCoordinates.Bottom +")");
+                    drawBordersOnOpenWinds(item);
                 }
             }
             Console.Read();
+        }
+
+        private static void drawBordersOnOpenWinds(Window window)
+        {
+            Rectangle myRect = new Rectangle();
+            WINDOWPLACEMENT windowPlacement = new WINDOWPLACEMENT();
+            windowPlacement.length = Marshal.SizeOf(windowPlacement);
+            Program.GetWindowPlacement(window.WindowHandle, ref windowPlacement);
+            myRect.X = window.WindowCoordinates.Left;
+            myRect.Y = window.WindowCoordinates.Top;
+            myRect.Width = (window.WindowCoordinates.Right - window.WindowCoordinates.Left)-1;
+            myRect.Height = (window.WindowCoordinates.Bottom - window.WindowCoordinates.Top)-1;
+                Console.WriteLine(window.WindowName);
+                new Border().HighLight(myRect, Program.BorderWidth);
         }
     }
 }
